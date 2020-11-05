74108e5ccb10df20b20198395ddbda814128833d
Miguel Martheyn <MiguelMartheyn@hotmail.com>
2020-11-05 18:39:46 -0500

    servicio Get
    queda listo el servicio Get.

--- Proyecto3- Reward Store/src/componentes/Header.js
+++ Proyecto3- Reward Store/src/componentes/Header.js
@@ -8,6 +8,9 @@ import Macbook from '../images/MacbookPro-x1.png'
 import AcerAspire from '../images/AcerAspire-x1.png';
 import Nintendo from '../images/Nintendo3DS-x1.png';
 import '../index.css'
+import GetUser from '../servicios/GetUser';
+import {user} from '../servicios/GetUser'
+
 
 
 function Header () {
@@ -22,59 +25,15 @@ function Header () {
       
     }
 
-    const requestInit = {
-      headers: {
-        "Content-Type": "application/json",
-        Authorization:
-          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZWRkOWU5OTQ0NGZlNDAwNmRhOTkyNGQiLCJpYXQiOjE1OTE1ODIzNjF9.-f40dyUIGFsBSB_PTeBGdSLI58I21-QBJNi9wkODcKk",
-      },
-    }
-
-const endPoint = "https://coding-challenge-api.aerolab.co/user/me";
-
 
 
     useEffect(() => {
-        if (!fetched) {
-          fetch(endPoint, requestInit)
-            .then((response) => response.json())
-            .then((userList) => {
-              setFetched(true);
-              setUsuarios({
-                name:userList.name,
-                points:userList.points
-              });
-              console.log(usuarios)
-            });
-        }
+
+        user({usuarios,setUsuarios,fetched,setFetched})
         
       }, [fetched]);
 
 
-    //   useEffect(() => {
-    //     var request = new XMLHttpRequest();
-
-    //     request.open('GET', 'https://coding-challenge-api.aerolab.co/user/me');
-        
-    //     request.setRequestHeader('Content-Type', 'application/json');
-    //     request.setRequestHeader('Accept', 'application/json');
-    //     request.setRequestHeader('Authorization', 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZWRkOWU5OTQ0NGZlNDAwNmRhOTkyNGQiLCJpYXQiOjE1OTE1ODIzNjF9.-f40dyUIGFsBSB_PTeBGdSLI58I21-QBJNi9wkODcKk');
-        
-    //     request.onreadystatechange = function () {
-    //       if (this.readyState === 4) {
-    //         console.log('Status:', this.status);
-    //         console.log('Headers:', this.getAllResponseHeaders());
-    //         // console.log('Body:', this.responseText);
-            
-    //         setUsuarios(this.responseText)
-    //       }
-    //     };
-    //     console.log(usuarios)
-    //     request.send();
-        
-        
-        
-    //   }, []);
     const styleLink = document.createElement("link");
     styleLink.rel = "stylesheet";
     styleLink.href = "https://cdn.jsdelivr.net/npm/semantic-ui/dist/semantic.min.css";
--- Proyecto3- Reward Store/src/servicios/GetUser.js
+++ Proyecto3- Reward Store/src/servicios/GetUser.js
@@ -1,15 +1,37 @@
-import React from "react";
-import "./index.css";
 
+  export  function user  ({usuarios,setUsuarios,fetched,setFetched}){
 
+    const requestInit = {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZWRkOWU5OTQ0NGZlNDAwNmRhOTkyNGQiLCJpYXQiOjE1OTE1ODIzNjF9.-f40dyUIGFsBSB_PTeBGdSLI58I21-QBJNi9wkODcKk",
+        },
+      }
+  
+  const endPoint = "https://coding-challenge-api.aerolab.co/user/me";
 
-
-function GetUser() {
-
+  
+  
+    if (!fetched) {
+      fetch(endPoint, requestInit)
+        .then((response) => response.json())
+        .then((userList) => {
+          setFetched(true);
+          setUsuarios({
+            name:userList.name,
+            points:userList.points
+          });
+          console.log(usuarios)
+        });
+    }
     
-  return (
-    
-  );
-}
+  
+
+     
+  }
+
+  
+
 
 
