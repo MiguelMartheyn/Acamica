f41dd09c0ba8c734444eb3bb8d49d9b9f71ea34b
Miguel Martheyn <MiguelMartheyn@hotmail.com>
2020-11-06 01:19:06 -0500

    paginacion
    se inicia configuracion de paginacion

--- Proyecto3- Reward Store/src/componentes/Cards.js
+++ Proyecto3- Reward Store/src/componentes/Cards.js
@@ -3,8 +3,10 @@ import SubMenu from "./SubMenu";
 import SemanticCard from "../semanticUiCard/SemanticCard";
 
 function Cards({ usuarios }) {
+  
+   const val = usuarios.products? usuarios.products.slice(0,16): []
   const cards = usuarios.products ? (
-    usuarios.products.map((card) => (
+    val.map((card) => (
       <SemanticCard
         name={card.name}
         image={card.img.url}
@@ -18,6 +20,7 @@ function Cards({ usuarios }) {
     <h1>No hay Productos disponibles</h1>
   );
 
+  console.log(val)
   return (
     <div>
       <div className="cards">{cards}</div>
