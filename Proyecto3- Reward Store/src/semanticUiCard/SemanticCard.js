11b520f029c0d6b972720f6a04c50562440c7663
Miguel Martheyn <MiguelMartheyn@hotmail.com>
2020-11-07 17:16:42 -0500

    estilos
    se agregan estilos a las card. Se agrega identificacion segun los puntos y el costo. Falta agregar funcionalidad

--- Proyecto3- Reward Store/src/semanticUiCard/SemanticCard.js
+++ Proyecto3- Reward Store/src/semanticUiCard/SemanticCard.js
@@ -1,17 +1,23 @@
 import React from "react";
-import { Card, Icon, Image } from "semantic-ui-react";
+import { Card, Icon, Image,Header } from "semantic-ui-react";
+import buyblue from '../images/buy-blue.svg'
+import coin from '../images/coin.svg'
 
 const CardExampleCard = ({ name, category, image ,cost,points,compra,setCompra}) => (
   <div className="card">
-    <Card>
+    <Card color='blue'>
       <Card.Content extra>
-        <a>
-          <Icon name="money" />
-           {cost}
-        </a>
+        
+         
+        
         <span className='span-semanticCard' onClick={()=>setCompra(true)}>
-          <Icon name="money" />
-           {points}
+        {cost >= points?
+          <Header >
+        
+        <div className='semanticCardNoCompraText'>Te faltan {cost-points}</div>
+        
+          <Image circular src={coin} /> </Header>: <img className='semanticCardCompra' src={buyblue}  />
+        }
         </span>
       </Card.Content>
       <Image src={image} wrapped ui={false} />
